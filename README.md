ba710d0c8b85b97fe0b45243bcba34e351f9e076
extremecoders-re <extremecoders@hotmail.com>
2022-08-18 12:36:27 +0100

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # firmware-mod-kit
+
 Firmware mod kit packaged for AttifyOS
+
+## Build instructions
+
+```
+$ cd src
+$ snapcraft
+```
---
+++ setup.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+show_usage() {
+  echo "Usage: $(basename $0) takes exactly 1 argument (install | uninstall)"
+}
+
+if [ $# -ne 1 ]
+then
+  show_usage
+  exit 1
+fi
+
+check_env() {
+  if [[ -z "${APM_TMP_DIR}" ]]; then
+    echo "APM_TMP_DIR is not set"
+    exit 1
+  
+  elif [[ -z "${APM_PKG_INSTALL_DIR}" ]]; then
+    echo "APM_PKG_INSTALL_DIR is not set"
+    exit 1
+  
+  elif [[ -z "${APM_PKG_BIN_DIR}" ]]; then
+    echo "APM_PKG_BIN_DIR is not set"
+    exit 1
+  fi
+}
+
+install() {
+  wget https://github.com/AttifyOS/firmware-mod-kit/releases/download/v0.99--8403a17/fmk_0.99-8403a17_amd64.snap -O $APM_TMP_DIR/fmk_0.99-8403a17_amd64.snap
+  sudo snap install $APM_TMP_DIR/nmap_7.92_amd64.snap --devmode
+  rm $APM_TMP_DIR/fmk_0.99-8403a17_amd64.snap
+
+  echo "#!/usr/bin/env sh" > $APM_PKG_BIN_DIR/extract-firmware.sh
+  echo "sudo fmk.extract-firmware" >> $APM_PKG_BIN_DIR/extract-firmware.sh
+  chmod +x $APM_PKG_BIN_DIR/extract-firmware.sh
+
+  echo "#!/usr/bin/env sh" > $APM_PKG_BIN_DIR/build-firmware.sh
+  echo "sudo fmk.build-firmware.sh" >> $APM_PKG_BIN_DIR/build-firmware.sh
+  chmod +x $APM_PKG_BIN_DIR/build-firmware.sh
+
+  echo "This package provides the following commands
+    - extract-firmware.sh
+    - build.firmware.sh"
+}
+
+uninstall() {
+  sudo snap remove fmk
+  rm $APM_PKG_BIN_DIR/extract-firmware.sh
+  rm $APM_PKG_BIN_DIR/build-firmware.sh
+}
+
+run() {
+  if [[ "$1" == "install" ]]; then 
+    install
+  elif [[ "$1" == "uninstall" ]]; then 
+    uninstall
+  else
+    show_usage
+  fi
+}
+
+check_env
+run $1
